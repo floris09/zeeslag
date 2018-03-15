87174d057f97d7500ab9be9430c362bee6746bd1
Rik Aikman <rikaikman@hotmail.com>
2018-03-15 17:17:00 +0100

    routes,  controller and resource for Game are setup

---
+++ app/Http/Resources/Game.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Game extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+//        return parent::toArray($request);
+        
+        //customize the response here:
+        return[
+            'id' => $this->id
+        ];
+    }
+}
