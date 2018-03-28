4af4f4b7ea524b159034bb244604edf19a3e2992
floris09 <florismeininger@gmail.com>
2018-03-28 10:20:16 +0200

    set up Bank project folders and add login

--- bank/index.php
+++ bank/index.php
@@ -1,2 +1,19 @@
-<?php
-echo "BANK";
+<?php require_once('./private/initialize.php'); ?>
+
+<!DOCTYPE html>
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href=<?= './styles/style.css?'.time(); ?> />
+  <link rel="stylesheet" media="screen and (min-width: 900px)" href=<?= "./styles/widestyle.css?".time(); ?> >
+  <title>Flonani Bank</title>
+
+</head>
+
+<body id='login-body'>
+
+<?php include(SHARED_PATH . '/login.php'); ?>
+
+</body>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
