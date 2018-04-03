5d84aee1a9ab1366cf7f0cd84ce1a8bc94130b3e
Nena Ani <nenaani@MacBook-Pro-van-Nena.local>
2018-04-03 09:39:03 +0200

    style

---
+++ bank/public/styles/Overzicht.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <svg 
+ xmlns="http://www.w3.org/2000/svg"
+ xmlns:xlink="http://www.w3.org/1999/xlink"
+ width="67.751cm" height="38.118cm">
+<defs>
+<filter filterUnits="userSpaceOnUse" id="Filter_0" x="1664px" y="24px" width="238px" height="74px"  >
+    <feOffset in="SourceAlpha" dx="0" dy="3" />
+    <feGaussianBlur result="blurOut" stdDeviation="2.646" />
+    <feFlood flood-color="rgb(0, 0, 0)" result="floodOut" />
+    <feComposite operator="atop" in="floodOut" in2="blurOut" />
+    <feComponentTransfer><feFuncA type="linear" slope="0.35"/></feComponentTransfer>
+    <feMerge>
+    <feMergeNode/>
+    <feMergeNode in="SourceGraphic"/>
+  </feMerge>
+</filter>
+
+</defs>
+<rect  x="6060" y="0" width="1920" height="1080" fill="white" />
+<text kerning="auto" font-family="Avenir Next" fill="rgb(0, 0, 0)" font-size="40px" x="1273px" y="63.08px">Welkom&#32;Mw&#32;J&#46;&#32;Doe</text>
+<g filter="url(#Filter_0)">
+<path fill-rule="evenodd"  fill="rgb(238, 238, 238)"
+ d="M1670.000,27.000 L1895.000,27.000 L1895.000,88.000 L1670.000,88.000 L1670.000,27.000 Z"/>
+</g>
+<text font-family="Avenir Next" fill="rgb(232, 78, 15)" font-size="30px" x="1712px" y="65.92px"> <tspan dy="-1.2em">Uitloggen</tspan><tspan x="1712px" dy="1.2em"> </tspan>
+</text>
+<path fill-rule="evenodd"  fill="rgb(232, 78, 15)"
+ d="M-0.000,113.000 L1647.000,113.000 L1647.000,116.000 L-0.000,116.000 L-0.000,113.000 Z"/>
+<path fill-rule="evenodd"  fill="rgb(232, 78, 15)"
+ d="M29.000,198.000 L37.000,198.000 L37.000,236.000 L29.000,236.000 L29.000,198.000 Z"/>
+<text kerning="auto" font-family="Avenir Next" fill="rgb(0, 0, 0)" font-size="25px" x="51px" y="224.55px">Overzicht</text>
+<text kerning="auto" font-family="Avenir Next" fill="rgb(0, 0, 0)" font-size="25px" x="52px" y="264.55px">Opdrachten</text>
+<text kerning="auto" font-family="Avenir Next" fill="rgb(0, 0, 0)" font-size="25px" x="53px" y="292.55px">Producten</text>
+<text kerning="auto" font-family="Avenir Next" fill="rgb(0, 0, 0)" font-size="25px" x="52px" y="326.55px">Service</text>
+<path fill-rule="evenodd"  stroke="rgb(149, 149, 149)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none"
+ d="M232.000,319.000 L1647.000,319.000 L1647.000,904.000 L232.000,904.000 L232.000,319.000 Z"/>
+<path fill-rule="evenodd"  stroke="rgb(125, 125, 125)" stroke-width="0.22px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(238, 238, 238)"
+ d="M232.000,200.000 L1457.000,200.000 L1457.000,252.000 L232.000,252.000 L232.000,200.000 Z"/>
+<path fill-rule="evenodd"  stroke="rgb(125, 125, 125)" stroke-width="0.22px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(238, 238, 238)"
+ d="M1456.000,200.000 L1647.000,200.000 L1647.000,252.000 L1456.000,252.000 L1456.000,200.000 Z"/>
+<text font-family="Avenir Next" fill="rgb(163, 163, 163)" font-size="30px" x="1522px" y="232.06px">Zoek</text>
+<text font-family="Avenir Next" fill="rgb(163, 163, 163)" font-size="30px" x="267px" y="239.06px">Waar&#32;ben&#32;je&#32;naar&#32;opzoek&#63;</text>
+<text kerning="auto" font-family="Avenir Next" fill="rgb(0, 0, 0)" font-size="30px" x="269px" y="356.06px">Mw&#32;J&#46;&#32;Doe</text>
+<text kerning="auto" font-family="Avenir Next" fill="rgb(0, 0, 0)" font-size="25px" x="269px" y="397.55px">NL&#52;&#52;&#32;FNBQ&#32;&#48;&#48;&#48;&#49;&#32;&#49;&#50;&#51;&#52;&#32;&#53;&#54;</text>
+<text kerning="auto" font-family="Avenir Next" fill="rgb(0, 0, 0)" font-size="25px" x="1501px" y="353.55px">&#8364;&#49;&#46;&#52;&#57;&#56;&#44;&#57;&#51;</text>
+</svg>
+<head>
+<style>
+.Inloggen {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 1920px;
+  height: 1080px;
+  z-index: 19;
+}
+.Inloggen {
+  font-size: 30px;
+  font-family: "Avenir Next";
+  color: rgb(255, 255, 255);
+  line-height: 1.2;
+  position: absolute;
+  left: 292px;
+  top: 604.068px;
+  width: 128px;
+  height: 30px;
+  z-index: 18;
+}
+.Rechthoek_4 {
+  background-color: rgb(232, 78, 15);
+  position: absolute;
+  left: 141px;
+  top: 592px;
+  width: 428px;
+  height: 61px;
+  z-index: 17;
+}
+.Rechthoek_3 {
+  border-width: 1px;
+  border-color: rgb(232, 149, 115);
+  border-style: solid;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  left: 147px;
+  top: 521px;
+  width: 17px;
+  height: 17px;
+  z-index: 16;
+}
+.Rechthoek_2_kopiëren {
+  background-color: rgb(232, 149, 115);
+  position: absolute;
+  left: 141px;
+  top: 489px;
+  width: 428px;
+  height: 1px;
+  z-index: 15;
+}
+.Nog_geen_lid_van_FNB__Meld_je_aan_ {
+  font-size: 20px;
+  font-family: "Avenir Next";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  text-align: left;
+  position: absolute;
+  left: 141px;
+  top: 850.045px;
+  width: 343px;
+  height: 20px;
+  z-index: 14;
+}
+.Wachtwoord_vergeten_ {
+  font-size: 20px;
+  font-family: "Avenir Next";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  text-align: left;
+  position: absolute;
+  left: 140.437px;
+  top: 797.779px;
+  width: 220px;
+  height: 20px;
+  z-index: 13;
+}
+.Onthoud_mijn_gebruikersnaam {
+  font-size: 20px;
+  font-family: "Avenir Next";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  text-align: left;
+  position: absolute;
+  left: 179px;
+  top: 521.045px;
+  width: 292px;
+  height: 20px;
+  z-index: 12;
+}
+.Wachtwoord {
+  font-size: 20px;
+  font-family: "Avenir Next";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  text-align: left;
+  position: absolute;
+  left: 141.437px;
+  top: 433.779px;
+  width: 118px;
+  height: 15px;
+  z-index: 11;
+}
+.Rechthoek_2 {
+  background-color: rgb(232, 149, 115);
+  position: absolute;
+  left: 141px;
+  top: 377px;
+  width: 428px;
+  height: 1px;
+  z-index: 10;
+}
+.Gebruikersnaam {
+  font-size: 20px;
+  font-family: "Avenir Next";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  text-align: left;
+  position: absolute;
+  left: 142px;
+  top: 320.045px;
+  width: 154px;
+  height: 15px;
+  z-index: 9;
+}
+.Log_in_op_uw_account {
+  font-size: 20px;
+  font-family: "Avenir Next";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  text-align: center;
+  position: absolute;
+  left: 156.162px;
+  top: 249.92px;
+  width: 205px;
+  height: 19px;
+  z-index: 8;
+}
+.Welkom_terug {
+  font-size: 45px;
+  font-family: "Avenir Next";
+  color: rgb(0, 0, 0);
+  line-height: 1.2;
+  text-align: center;
+  position: absolute;
+  left: 156.427px;
+  top: 194.586px;
+  width: 296px;
+  height: 45px;
+  z-index: 7;
+}
+.Inloggen {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 8176px;
+  height: 1080px;
+  z-index: 5;
+}
+.Rechthoek_1 {
+  background-color: rgb(232, 78, 15);
+  position: absolute;
+  left: -1px;
+  top: 113px;
+  width: 624px;
+  height: 3px;
+  z-index: 4;
+}
+
+</style>
+</head>
+</body>
+</html>
+
